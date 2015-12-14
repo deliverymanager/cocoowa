fb590d3de11f8b47b170fac7101e6659916bbe69
kostazj <kostazj@gmail.com>
2015-12-14 12:25:50 +0200

    Adding send comments functionality

--- www/js/controllers/comments.js
+++ www/js/controllers/comments.js
@@ -1,3 +1,21 @@
-angular.module('cocoowa').controller('CommentsController',function(){
+angular.module('cocoowa').controller('CommentsController', function ($scope, Comments) {
+
+  //Initializing the comments  parameters object
+  $scope.commentsObject = {
+    "name": "",
+    "email": "",
+    "comments": ""
+  };
+
+  //Validating the commentsParametersObject object
+
+  $scope.sendComment = function(){
+
+
+
+  };
+
+  //POST request
+  Comments.send()
 
 });
