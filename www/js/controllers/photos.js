344b64c156b1c2aa84d95bda342daf4f0a202c55
kostazj <kostazj@gmail.com>
2015-12-14 22:38:52 +0200

    Error with swiper

--- www/js/controllers/photos.js
+++ www/js/controllers/photos.js
@@ -1,13 +1,18 @@
 angular.module('cocoowa')
-  .controller('PhotosController', function(){
+  .controller('PhotosController', function($scope){
+
+    console.log("PhotosController loaded!");
 
     /*SWIPER*/
-    var photoSwiper = new Swiper ('.swiper-container', {
-      // Optional parameters
-      direction: 'horizontal',
-      loop: true,
-      effect: 'fade',
-      autoplay: 4000
-    });
+    $scope.initSwiper = function(){
+      var photoSwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        effect: 'fade',
+        autoplay: 4000
+      });
+    };
+
 
   });
